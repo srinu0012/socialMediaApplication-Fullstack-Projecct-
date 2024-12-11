4e490da2262f08842d133e555fd0b32445957dba
srinu0012 <sniperstarttt@gmail.com>
2024-12-11 17:27:52 +0530

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { lazy,Suspense } from "react";
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
 
+import  Profile from "./components/profile/profile"
 
 const Login=lazy(()=>import("./components/login/login"))
-const Profile=lazy(()=>import("./components/profile/profile"))
+
 const Home=lazy(()=>import("./pages/home/home"))
 const  FriendsProfile =lazy(()=>import("./components/friendProfile/friendProfile"))
 const UnderConstruction=lazy(()=>import("./components/underWorking/underworking"))
@@ -33,7 +34,7 @@ const[islogin,setislogin]=useState(false)
          <Routes>
         <Route path="/" element={<Suspense fallback="loadind...."><Login/></Suspense>}></Route>
         <Route path="/Home" element={<Suspense fallback="loadind...."><Home/></Suspense>}></Route>
-        <Route path="/Home/profile" element={<Suspense fallback="loadind...."><Profile/></Suspense>}></Route>
+        <Route path="/Home/profile" element={<Profile/>}></Route>
         <Route path="/friends/:id" element={<Suspense fallback="loadind...."><FriendsProfile /></Suspense>} />
         <Route path="/Home/videos" element={<Suspense fallback="loadind...."><Videos/></Suspense>}/>
         <Route path="*" element={<Suspense fallback="loadind...."><UnderConstruction/></Suspense>}></Route>
